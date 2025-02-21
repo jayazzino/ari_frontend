37976051e68a642436c7c1967b61d1f28e1fae1c
andreainternullo <168184246+andreainternullo@users.noreply.github.com>
2025-02-21 10:42:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,53 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ari-frontend-v2';
+export class AppComponent implements AfterViewInit{
+  date: any;
+  now: any;
+  targetDate: any = new Date(2025, 2, 19);
+  targetTime: any = this.targetDate.getTime();
+  difference!: number;
+  months: Array<string> = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  currentTime: any = `${
+    this.months[this.targetDate.getMonth()]
+  } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
+
+  @ViewChild('days', { static: true }) days!: ElementRef;
+  @ViewChild('hours', { static: true }) hours!: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes!: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds!: ElementRef;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+      !isNaN(this.days.nativeElement.innerText)
+        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
+        : (this.days.nativeElement.innerHTML = `<img src="https://i.gifer.com/VAyR.gif" />`);
+    }, 1000);
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+    this.days.nativeElement.innerText = Math.floor(this.difference);
+    this.hours.nativeElement.innerText = 23 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
 }
